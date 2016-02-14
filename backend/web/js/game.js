0f2e9672918c83d96fb04374426c8c6e772eaa3f
den2134 <qwe100com@mail.ru>
2016-02-14 18:20:35 +0200

    Game moveTo() v0.02

--- backend/web/js/game.js
+++ backend/web/js/game.js
@@ -5,12 +5,37 @@ function done(){
 function move(player, target){
     var value = target.val();
 
-    console.log(value);
+    var reg = /(left:|right:|top:|bottom:)+\+=[0-9]*px/gi;
+    value = value.replace(/\r|\n/g, '');
 
-    var reg = /left:|right:|top:|bottom:|/gi;
-    var tTegs = value.split(reg);
+    var tTegs = value.split(',');
 
     console.log(tTegs);
 
-    player.animate({}, 1000);
+    var tVal = [];
+    for(var i=0; i<tTegs.length; i++){
+        tVal[i] = tTegs[i].split(':');
+    }
+
+    console.log(tVal);
+
+    for(var i=0; i<tVal.length; i++) {
+        //tVal[i][0] = tVal[i][0].replace('right','-left');
+        var teg = tVal[i][0];
+        var val = tVal[i][1];
+        moveTo(player, teg, val);
+      }
+}
+
+function moveTo(object, teg, val){
+    switch(teg){
+        case 'left':
+            object.animate({
+                left:val,
+            },1000);break;
+        case  'top':
+            object.animate({
+                'top':val,
+            },1000);break;
+    }
 }
