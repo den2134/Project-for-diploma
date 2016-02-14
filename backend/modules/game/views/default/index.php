2f65af82474a5c7e22d43583880a802ffe49d098
den2134 <qwe100com@mail.ru>
2016-02-14 23:58:15 +0200

    Game v0.10

--- backend/modules/game/views/default/index.php
+++ backend/modules/game/views/default/index.php
@@ -2,8 +2,11 @@
 
 <div class="game-default-index">
     <div class="container">
+        <div id="check"></div>
         <div id="ground" class="col-md-10 col-sm-4 col-lg-12">
             <div id="player"></div>
+            <div id="block1"></div>
+            <div id="finish"></div>
         </div>
         <div class="form-group col-md-6 myform">
             <textarea type="text" class="form-control" id="scr" rows="5" name="user_scr" placeholder="Add script"></textarea>
--- backend/web/js/game.js
+++ backend/web/js/game.js
@@ -1,3 +1,8 @@
+window.setInterval(function() {
+    collision($('#player'),$('#block1')) ? die($('#player')) : false;
+    collision($('#player'),$('#finish')) ? is_win() : false;
+}, 200);
+
 function done(){
     move($('#player'),$('#scr'));
 }
@@ -5,26 +10,20 @@ function done(){
 function move(player, target){
     var value = target.val();
 
-    var reg = /(left:|right:|top:|bottom:)+\+=[0-9]*px/gi;
     value = value.replace(/\r|\n/g, '');
-
     var tTegs = value.split(',');
-
-    console.log(tTegs);
-
     var tVal = [];
+
     for(var i=0; i<tTegs.length; i++){
         tVal[i] = tTegs[i].split(':');
     }
 
-    console.log(tVal);
-
     for(var i=0; i<tVal.length; i++) {
-        //tVal[i][0] = tVal[i][0].replace('right','-left');
         var teg = tVal[i][0];
         var val = tVal[i][1];
+
         moveTo(player, teg, val);
-      }
+    }
 }
 
 function moveTo(object, teg, val){
@@ -32,10 +31,77 @@ function moveTo(object, teg, val){
         case 'left':
             object.animate({
                 left:val,
-            },1000);break;
+            },1000);
+            break;
         case  'top':
             object.animate({
                 'top':val,
             },1000);break;
     }
 }
+// WORK WITH COLLISIONS
+function collision($div1, $div2) {
+    var x1 = $div1.offset().left;
+    var y1 = $div1.offset().top;
+    var h1 = $div1.outerHeight(true);
+    var w1 = $div1.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $div2.offset().left;
+    var y2 = $div2.offset().top;
+    var h2 = $div2.outerHeight(true);
+    var w2 = $div2.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function die(object){
+    object.css({'background-color':'red','transition':'0.8s','left':'+=0'});
+    setTimeout(function() {
+        object.animate({
+            'transition':'0s',
+            left: '0px',
+            top: '0px',
+        });
+        object.css({'background-color': '#ffd4a8'});
+    },800);
+    return false;
+}
+
+function is_win(){
+    ($('#ground')).css({'background-color': 'green'});
+}
+
+
+
+/*var colLeft = val.replace(/px/g,'');
+ var colLeft = colLeft.replace(/\+=|-=/g,'');
+
+ if(/\+=/.test(val)) {
+ var colPlayerLeft = player.offset().left + parseInt(colLeft);
+ var colPlayerTop = player.offset().top + parseInt(colLeft);
+ }
+ else {
+ var colPlayerLeft = player.offset().left - parseInt(colLeft);
+ var colPlayerTop = player.offset().top - parseInt(colLeft);
+ }
+
+ if(teg == 'left') {
+ if (($('#block1').offset().left - $('#block1').outerWidth(true)) <= colPlayerLeft) {
+ die(player);
+ }
+ }
+ else(teg == 'top')
+ {
+ console.log($('#block1').offset().top + $('#block1').outerHeight(true));
+ console.log(colPlayerTop);
+ if (($('#block1').offset().top - $('#block1').outerHeight(true)) <= colPlayerTop) {
+ die(player);
+ }
+ }*/
