416999bbf32c179847e37ac401ff0ff64be79ab8
den2134 <qwe100com@mail.ru>
2016-02-22 22:17:51 +0200

    RPG v.002

--- backend/modules/RPG/views/default/index.php
+++ backend/modules/RPG/views/default/index.php
@@ -1,7 +1,8 @@
 <div id="log"></div>
 <div id="background" class="col-md-12 col-sm-12 col-lg-12">
     <div id="player_items">
-        <img id="role_player" src="http://2.bp.blogspot.com/-4RH6yfq_BVc/UXEIhkQcAtI/AAAAAAAAGOA/1FjtsdagIv0/s400/minecraft-pixel-art-batman.png" alt="">
+        <!--<img id="role_player" src="http://2.bp.blogspot.com/-4RH6yfq_BVc/UXEIhkQcAtI/AAAAAAAAGOA/1FjtsdagIv0/s400/minecraft-pixel-art-batman.png" alt="">-->
         <div class="obj"></div>
     </div>
+    <div class="enemy"></div>
 </div>
--- backend/web/js/rpgGame.js
+++ backend/web/js/rpgGame.js
@@ -1,44 +1,76 @@
 /**
  * Created by Денис on 22.02.2016.
  */
-window.setInterval(function() {
-    var position = $('#role_player').position();
-    $('#player_items').css({'left':position.left, 'top':position.top});
 
-}, 200);
+window.setInterval(function() {
+    moveEnemy($('.enemy'), $('#player_items'));
+    if(collision($('.obj'), $('.enemy')))
+        console.log('DONE');
+    else
+        console.log('NOPE');
+}, 1000);
+
 $( document ).on( "mousemove", function( event ) {
     $( "#log" ).text( "pageX: " + event.pageX + ", pageY: " + event.pageY );
 });
 
-$(('#background')).click(function(e){
+$('#background').click(function(e){
+    var offset = $("#player_items").offset();
+
     $('.obj').animate({
-        left: e.pageX-130,
-        top: e.pageY-110
+        left: e.pageX - offset.left,
+        top: e.pageY - offset.top
     },600);
 
     setTimeout(function(){
         $('.obj').remove();
-    }, 300);
+    }, 600);
 
     setTimeout(function(){
         $('#player_items').append('<div class="obj">');
-    }, 300);
+    }, 600);
 });
 
 $(document).keydown(function(e){
-    var position = $('#role_player').position();
+    var position = $('#player_items').position();
+
     switch(e.keyCode){
         case 37:
-            $('#role_player').css('left',position.left-20+'px');
+            $('#player_items').css('left',position.left-20+'px');
             break;
         case 38:
-            $('#role_player').css('top',position.top-20+'px');
+            $('#player_items').css('top',position.top-20+'px');
             break;
         case 39:
-            $('#role_player').css('left',position.left+20+'px');
+            $('#player_items').css('left',position.left+20+'px');
             break;
         case 40:
-            $('#role_player').css('top',position.top+20+'px');
+            $('#player_items').css('top',position.top+20+'px');
             break;
     }
 });
+
+function moveEnemy(enemy, player){
+    $(enemy).css({'left':player.offset().left, 'top':player.offset().top});
+}
+
+function collision($div1, $div2) {
+    var x1 = $div1.offset().left;
+    var y1 = $div1.offset().top;
+    var h1 = $div1.outerHeight(true);
+    var w1 = $div1.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $div2.offset().left;
+    var y2 = $div2.offset().top;
+    var h2 = $div2.outerHeight(true);
+    var w2 = $div2.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
+
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+        return false;
+    } else {
+        return true;
+    }
+}
